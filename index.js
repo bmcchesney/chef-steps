8f41227bc21bd9762b3b334eb63e874eba545277
Brett McChesney <bmcchesney@valantmed.com>
2015-10-03 15:08:35 -0700

    swap out httpdispatcher for express

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+ * A simple express app to handle some basic api routes.
+ */
+
+var express = require('express');
+var common = require('./common.js');
+
+var PORT_NUMBER = 3000;
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send("A simple http server for chef-steps coding challenge.\n");
+});
+
+app.post('/remove-duplicates', function(req, res) {
+  var data = '';
+
+  req.on('readable', function() {
+    data += req.read();
+  });
+
+  req.on('end', function() {
+    var res_body = JSON.parse(data);
+    res.send(JSON.stringify(common.removeDups(res_body)));
+  });
+  
+});
+
+app.listen(process.env.PORT || PORT_NUMBER);
